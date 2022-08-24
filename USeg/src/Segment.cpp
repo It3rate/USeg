98083d074d3899376a8110f07f9df1c1840fca25
Robin Debreuil <it3rate@gmail.com>
2022-08-24 18:03:44 -0500

    Moving to home.

--- USeg/src/Segment.cpp
+++ USeg/src/Segment.cpp
@@ -1,15 +1,15 @@
 #include "Segment.h"
+#include <limits>
 using namespace umath;
 
 const Segment DEFAULT_UNIT{ 0.0, 1.0 };
 const Segment DEFAULT_UNOT{ 0.0, -1.0 };
 
-Segment::Segment(const double start, const double end, const int trait_index) :
-    i_start_(start), r_end_(end), trait_index_(trait_index)
+Segment::Segment(const double i_start, const double r_end) : i_start_(i_start), r_end_(r_end)
 {
 }
 
-Segment::Segment(const double end) : i_start_(-end), r_end_(end), trait_index_(0)
+Segment::Segment(const double r_end) : i_start_(-r_end), r_end_(r_end)
 {
 }
 
@@ -33,8 +33,8 @@ void Segment::Div(const Segment& _right) {
 
     // shouldn't have to deal with NaNs or div by zero eventually
     if (isnan(_right_r) || isnan(_right_i)) { // set NaN result
-        this->r_end_ = NAN;
-        this->i_start_ = NAN;
+        this->r_end_ = std::numeric_limits<double>::quiet_NaN();
+        this->i_start_ = std::numeric_limits<double>::quiet_NaN();
     }
     else if ((_right_i < 0 ? -_right_i : +_right_i)
         < (_right_r < 0 ? -_right_r : +_right_r)) { // |_right_i| < |_right_r|
@@ -46,15 +46,15 @@ void Segment::Div(const Segment& _right) {
         this->r_end_ = _tmp;
     }
     else if (_right_i == 0) {
-        this->r_end_ = NAN;
-        this->i_start_ = NAN;
+        this->r_end_ = std::numeric_limits<double>::quiet_NaN();
+        this->i_start_ = std::numeric_limits<double>::quiet_NaN();
     }
     else { // 0 < |_right_r| <= |_right_i|
         double _wr = _right_r / _right_i;
         double _wd = _right_i + _wr * _right_r;
         if (isnan(_wd) || _wd == 0) {
-            this->r_end_ = NAN;
-            this->i_start_ = NAN;
+            this->r_end_ = std::numeric_limits<double>::quiet_NaN();
+            this->i_start_ = std::numeric_limits<double>::quiet_NaN();
         }
         else { // compute representable result
             double _tmp = (this->r_end_ * _wr + this->i_start_) / _wd;
@@ -62,6 +62,5 @@ void Segment::Div(const Segment& _right) {
             this->r_end_ = _tmp;
         }
     }
-
 
 }
