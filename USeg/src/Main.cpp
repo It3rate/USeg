3f7fd5b44876670f9a678838bd3aa56fb9429690
Robin Debreuil <it3rate@gmail.com>
2022-08-24 13:32:40 -0500

    Added some math to segment.

--- USeg/src/Main.cpp
+++ USeg/src/Main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char* argv[])
 	std::cout << "hello\n";
 
 	const umath::Segment seg0(10.3,20.0);
-	std::cout << "start = " << seg0.start_ << "\n";
-	std::cout << "end = " << seg0.end_ << "\n";
+	std::cout << "start = " << seg0.i_start_ << "\n";
+	std::cout << "end = " << seg0.r_end_ << "\n";
 
     return 0;
 }
--- USeg/src/Segment.h
+++ USeg/src/Segment.h
@@ -8,14 +8,19 @@ namespace umath
 	class Segment
 	{
 	public:
-		Number<double> start_;
-		Number<double> end_;
+		double i_start_;
+		double r_end_;
 		int trait_index_;
 
 		Segment(double start, double end , int trait_index = 0);
 		explicit Segment(double end);
+
+		constexpr void Add(const Segment& _right);
+		constexpr void Sub(const Segment& _right);
+		constexpr void Mul(const Segment& _right);
+		void Div(const Segment& _right);
 	};
 	extern const umath::Segment kDefaultUnit;
-	extern const umath::Segment kDefaultUnot;
+	extern const umath::Segment kDefaultUnot; 
 }
 
